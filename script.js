b17d640b487c603b9b7c21c7a2b128d472aaa924
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-12 05:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading and trailing whitespace
+  str = str.trim();
+
+  // If string is empty, return empty string
+  if (str === '') return '';
+
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
+
+  // If no space is found, return the whole string
+  if (spaceIndex === -1) return str;
+
+  // Otherwise, return substring up to the first space
+  return str.slice(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
